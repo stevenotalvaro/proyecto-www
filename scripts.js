253fb9d877b4bc0462c24f72a1eb35a025530758
Steven Otalvaro <fabiosteven1998.12@gmail.com>
2020-08-07 23:09:39 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const iconMenu = document.getElementById('icon-menu');
+const mainMenu = document.getElementById('main-menu');
+
+iconMenu.addEventListener('click', () => mainMenu.classList.toggle('menu--show'));
